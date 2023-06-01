cabfff316fe0409cce3e4b19d82e661c61c6b694
GuillaumeVandekerckhove <guillaume.vandekerckhove@outlook.com>
2023-06-01 15:04:22 +0200

    add function to create the boxplots per parameter.

--- pygada/explorative_data_analysis/boxplots.py
+++ pygada/explorative_data_analysis/boxplots.py
@@ -1,8 +1,9 @@
+import pandas as pd
 from loguru import logger
-import matplotlib as plt
+from matplotlib import pyplot as plt
 import seaborn as sns
 
-def boxplots(inputdf, save=False):
+def boxplots(inputdf, outputpath=None, save=False):
     """Create a box plot per parameter that is in the dataset. The y-scale contains the concentrations in log.
     The plot can be saved as a png file.
 
@@ -10,70 +11,34 @@ def boxplots(inputdf, save=False):
     ----------
     inputdf : dataframe
         The input dataframe.
+    outputpath : str
+        The folder where the results will be saved.
+    save : Boolean
+        The option to save the result.
 
     Returns
     -------
     None
     """
 
+    unit = inputdf['unit'].unique()[0]
+
     plt.figure(figsize=(20, 10))
     plt.xticks(fontsize=20)
     plt.yticks(fontsize=20)
-    plot = sns.boxplot(x="parameter", y="meetwaarde", data=inputdf, color="lightblue")
+    plot = sns.boxplot(x="parameter", y="value", data=inputdf, color="lightblue")
     plot.set(ylim=0.01)
     plot.set(yscale="log")
     plot.set_xlabel("Parameter", fontsize=20)
+    plot.set_ylabel(f"Value in log {unit}", fontsize=20)
     plt.xticks(rotation=90)
     plt.tight_layout()
-    if save:
-        plt.savefig(f"{outputpath}/boxplots/boxplot_parameters_{parameter}.png")
+    plt.show()
+    # if save:
+        # plt.savefig(f"{outputpath}/boxplot_parameters.png")
     plt.clf()
 
     logger.info(f"Plotting the box plot per parameter.")
 
-def plot_dataset(self, inputdf=pd.DataFrame, outputpath=str, parameter=str, source=str, max_value=float):
-    """Create a box plot per parameter that is in the dataset. The y-scale with the concentrations is in log.
-    The plot is saved as a png file.
-
-    :param inputdf: The input dataframe.
-    :param outputpath: Part of the path where the user want to store the result png.
-    :param parameter: The chosen parameter(s), it/they are part of the path to the result png.
-    :param source: The source from where the data comes from.
-    :param max_value: The maximum value of the concentration of the parameters in the dataset.
-    :return: None.
-    """
-
-    logger.info(f"Start plotting the box plot per parameter.")
-
-    df = inputdf
-    if source == "VMM":
-        parameters = ['Temperatuur (veldmeting)', 'Geleidingsvermogen 20°C', 'Geleidingsvermogen 20Â°C',
-                      'Zuurgraad pH (veldmeting)']
-        for i in parameters:
-            df = df[df["parameter"] != i]
-    if source == 'pydov':
-        df = df.rename(columns={"waarde": "meetwaarde"})
-
-    plt.figure(figsize=(20, 10))
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
-    plot = sns.boxplot(
-        x="parameter", y="meetwaarde", data=df, color="lightblue"
-    )
-    plot.set(ylim=(0.01, max_value))
-    plot.set(yscale="log")
-    #todo: change ylabel to unit from input data.
-    if source == "pydov":
-        plot.set_xlabel("Parameter", fontsize=20)
-        plot.set_ylabel("Concentration", fontsize=20)
-    if (source == "VMM") | (source == "gw_OVAM"):
-        plot.set_xlabel("Parameter", fontsize=20)
-        plot.set_ylabel("Concentration (ng/l)", fontsize=20)
-    if source == "soil_OVAM":
-        plot.set_xlabel("Parameter", fontsize=20)
-        plot.set_ylabel("Concentration (ng/kg ds)", fontsize=20)
-
-    plt.xticks(rotation=90)
-    plt.tight_layout()
-    plt.savefig(f"{outputpath}/boxplots/boxplot_parameters_{parameter}.png")
-    plt.clf()
+df = pd.read_csv('C:/Users/vandekgu/OneDrive - Vlaamse overheid - Office 365/Documenten/PycharmProjects/pygada/pygada/test_data/results/PFAS_gw_VMM.csv')
+boxplots(df)
