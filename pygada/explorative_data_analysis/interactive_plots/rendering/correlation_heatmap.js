8fa8bce5ee88abd4b591d74572e48866d548d8be
GuillaumeVandekerckhove <guillaume.vandekerckhove@outlook.com>
2023-06-08 15:10:59 +0200

    Added files for interactive plot as HTML

---
+++ pygada/explorative_data_analysis/interactive_plots/rendering/correlation_heatmap.js
@@ -0,0 +1,143 @@
+document.addEventListener('DOMContentLoaded', function() {
+Highcharts.chart('correlation_heatmap',
+{
+  chart: {
+  margin: [40,
+null,
+80,
+null],
+  marginBottom: 80,
+  marginTop: 40,
+  plotBorderWidth: 1,
+  type: 'heatmap'
+},
+  colorAxis: {
+  max: 1,
+  min: 0,
+  reversed: false,
+  maxColor: '#00C1FF',
+  minColor: '#FFFFFF'
+},
+  legend: {
+  align: 'right',
+  layout: 'vertical',
+  margin: 0,
+  symbolHeight: 280,
+  verticalAlign: 'top',
+  y: 25
+},
+  series: [{
+  data: [{
+  value: 1.0,
+  x: 0,
+  y: 0
+},
+{
+  value: 0.2,
+  x: 0,
+  y: 1
+},
+{
+  value: 0.33,
+  x: 0,
+  y: 2
+},
+{
+  value: 0.37,
+  x: 0,
+  y: 3
+},
+{
+  value: 0.2,
+  x: 1,
+  y: 0
+},
+{
+  value: 1.0,
+  x: 1,
+  y: 1
+},
+{
+  value: 0.06,
+  x: 1,
+  y: 2
+},
+{
+  value: 0.6,
+  x: 1,
+  y: 3
+},
+{
+  value: 0.33,
+  x: 2,
+  y: 0
+},
+{
+  value: 0.06,
+  x: 2,
+  y: 1
+},
+{
+  value: 1.0,
+  x: 2,
+  y: 2
+},
+{
+  value: 0.18,
+  x: 2,
+  y: 3
+},
+{
+  value: 0.37,
+  x: 3,
+  y: 0
+},
+{
+  value: 0.6,
+  x: 3,
+  y: 1
+},
+{
+  value: 0.18,
+  x: 3,
+  y: 2
+},
+{
+  value: 1.0,
+  x: 3,
+  y: 3
+}],
+  name: 'Correlation',
+  dataLabels: {
+  enabled: true
+},
+  tooltip: {
+  headerFormat: '<em>Parameter {point.key}</em><br/>'
+},
+  type: 'heatmap'
+}],
+  title: {
+  text: 'Correlation per parameter'
+},
+  xAxis: {
+  categories: ['PFOS',
+'PFHxS',
+'PFNA',
+'PFOA'],
+  title: {
+  text: 'Parameter'
+}
+},
+  yAxis: {
+  categories: ['PFOS',
+'PFHxS',
+'PFNA',
+'PFOA'],
+  reversed: true,
+  title: {
+  text: 'Parameter'
+}
+}
+},
+);
+});
